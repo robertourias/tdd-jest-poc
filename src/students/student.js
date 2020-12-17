95c6a84ebd457f832f2dffd925cfc925fa223afa
Roberto Nicoletti <roberto.urias@gmail.com>
2020-12-17 18:35:35 -0300

    Ajustando segundo teste no TDD

--- src/students/student.js
+++ src/students/student.js
@@ -1,6 +1,12 @@
 module.exports = {
-    sum: notes => notes.reduce((acc, note) => {
-        acc += note;
-        return acc;
-    }, 0)
+    sum: notes => {
+        const minValue = Math.min(...notes);
+        if (minValue === 0) {
+            return 0;
+        }
+        return notes.reduce((acc, note) => {
+            acc += note;
+            return acc;
+        }, 0)
+    }
 }
