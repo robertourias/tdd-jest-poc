df7e25040d8c6617b07c72e45cf6e7dc45593ac7
Roberto Nicoletti <roberto.urias@gmail.com>
2020-12-17 18:37:40 -0300

    Refatornando condicional

--- src/students/student.js
+++ src/students/student.js
@@ -1,12 +1,11 @@
 module.exports = {
     sum: notes => {
-        const minValue = Math.min(...notes);
-        if (minValue === 0) {
-            return 0;
+        if (Math.min(...notes)) {
+            return notes.reduce((acc, note) => {
+                acc += note;
+                return acc;
+            }, 0)
         }
-        return notes.reduce((acc, note) => {
-            acc += note;
-            return acc;
-        }, 0)
+        return 0;
     }
 }
